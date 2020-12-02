9d756487921aa6cb6c624a32c6d8f19f909bb0f6
Lars Kellogg-Stedman <lars@redhat.com>
2020-12-02 08:09:24 -0500

    run createrepo after reposync

--- reposync-daily.sh
+++ reposync-daily.sh
@@ -21,7 +21,25 @@ LOCKFILE="$REPODIR/.lock"
 	if [[ $? -eq 0 ]]; then
 		echo "reposync completed"
 	else
-		echo "reposync failed"
+		echo "ERROR: reposync failed" >&2
+		exit 1
 	fi
+
+	for repo in /srv/repos/*; do
+		if ! [[ -d $repo/Packages ]]; then
+			continue
+		fi
+
+		echo "generating repo metadata for $repo"
+		if [[ -f $repo/comps.xml ]]; then
+			comps="-g comps.xml"
+		else
+			comps=""
+		fi
+
+		createrepo --update $comps $repo
+	done
+
+	echo "all reposync operations complete"
 
 ) 2>&1 | logger -i -t reposync-daily -p user.info
